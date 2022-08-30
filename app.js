66c3a953310a2f8beadd01de52142d77f3d7e946
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2022-08-30 11:22:54 +0900

    moveTo and lineTo added

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ canvas.width = 800;
 canvas.height = 800;
 
 //ctx.fillRect(0,0,200,400);
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill();
+// 사각형 만드는 지름길
+// ctx.rect(50, 50, 100, 100);
+// ctx.rect(150, 150, 100, 100);
+// ctx.rect(250, 250, 100, 100);
+// ctx.fill(); 
 
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle="red";
-ctx.fill();
+// ctx.beginPath();
+// ctx.rect(350, 350, 100, 100);
+// ctx.rect(450, 450, 100, 100);
+// ctx.fillStyle="red";
+// ctx.fill();
+
+ctx.moveTo(50,50);
+ctx.lineTo(150,50);
+ctx.lineTo(150,150);
+ctx.lineTo(50,150);
+ctx.lineTo(50,50);
+ctx.stroke(); //선을 그음(채우지 않음)
+ctx.fill(); // 채워줌
