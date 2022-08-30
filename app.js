c2b9afd17d7a62f7a8d7b4c0dbb2889fa0594e92
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2022-08-30 13:57:29 +0900

    Drawing Project One added

--- app.js
+++ app.js
@@ -16,10 +16,24 @@ canvas.height = 800;
 // ctx.fillStyle="red";
 // ctx.fill();
 
-ctx.moveTo(50,50);
-ctx.lineTo(150,50);
-ctx.lineTo(150,150);
-ctx.lineTo(50,150);
-ctx.lineTo(50,50);
-ctx.stroke(); //선을 그음(채우지 않음)
-ctx.fill(); // 채워줌
+// 선긋기 예제
+// ctx.moveTo(50,50);
+// ctx.lineTo(150,50);
+// ctx.lineTo(150,150);
+// ctx.lineTo(50,150);
+// ctx.lineTo(50,50);
+// ctx.stroke(); //선을 그음(채우지 않음)
+// ctx.fill(); // 채워줌
+
+
+// 집만들기 예제
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+//ctx.lineWidth = 2;
+//ctx.strokeRect(300, 300, 50, 100);
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+ctx.moveTo(200,200);
+ctx.lineTo(325,100);
+ctx.lineTo(450, 200);
+ctx.fill();
